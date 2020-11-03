fdca59a6cd582e0906ba7a4fb2a342c22e392eee
Filip Dalväg <filip.dalvag@vgregion.se>
2020-11-03 11:19:18 +0100

    Now with JSON output!

--- tests/Resources/validate_html.py
+++ tests/Resources/validate_html.py
@@ -19,13 +19,17 @@ def check_w3c_valid(url):
     headers = {'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; \
                +http://www.google.com/bot.html)',
                'Content-type': 'text/html; charset=utf-8'}
-    w3c_response = requests.post(url=w3c_url, headers=headers, data=html)
+    params = {'out': 'json'}
+    w3c_response = requests.post(url=w3c_url,
+                                 headers=headers,
+                                 data=html,
+                                 params=params)
     return w3c_response
 
 
 """
 If file is executed on itself then call a definition,
-mostly for testing purposes
+mostly for testing purposes (sorry Säffle)
 """
 if __name__ == '__main__':
     print(check_w3c_valid('https://www.saffle.se/').text)
