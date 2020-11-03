f628dd1de26c0c284d34da3fc0d2828894bb2b17
Filip Dalväg <filip.dalvag@vgregion.se>
2020-11-03 11:01:18 +0100

    Linting

--- tests/Resources/validate_html.py
+++ tests/Resources/validate_html.py
@@ -1,19 +1,31 @@
+"""
+Validates HTML
+"""
 import requests
 
-def check_w3c_valid(url):
-    # Get our markup
-    r = requests.get(url, timeout=20)
 
-    html = r.text.encode('utf-8')
+def check_w3c_valid(url):
+    """
+    Gets the HTML from an url and posts that URL to the w3c Validator
+    """
+
+    # Get our markup
+    response = requests.get(url, timeout=20)
+
+    html = response.text.encode('utf-8')
 
     # Post to w3c
-    w3cURL = 'https://validator.w3.org/nu/'
-    headers = {'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', 'Content-type': 'text/html; charset=utf-8'}
-    w3cResponse = requests.post(url = w3cURL, headers=headers, data=html)
-    return w3cResponse
+    w3c_url = 'https://validator.w3.org/nu/'
+    headers = {'user-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; \
+               +http://www.google.com/bot.html)',
+               'Content-type': 'text/html; charset=utf-8'}
+    w3c_response = requests.post(url=w3c_url, headers=headers, data=html)
+    return w3c_response
+
 
 """
-If file is executed on itself then call a definition, mostly for testing purposes
+If file is executed on itself then call a definition,
+mostly for testing purposes
 """
 if __name__ == '__main__':
     print(check_w3c_valid('https://www.saffle.se/').text)
