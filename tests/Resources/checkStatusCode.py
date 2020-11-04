3564c866a4762ff2c227f3188c41482f84f2b920
Filip Dalväg <filip.dalvag@vgregion.se>
2020-11-04 08:42:21 +0100

    Some lint-fix, comments and cleanup. Loggs errors and makes it able to be run by executing the file directly.

--- tests/Resources/checkStatusCode.py
+++ tests/Resources/checkStatusCode.py
@@ -1,12 +1,28 @@
+"""
+Check if url returns a http status code in the 200 range.
+"""
+import logging
 import requests
 
 
 def check_status_code(url="https://api.github.com/user"):
+    """
+    Checks the HTTP error code for given URL. Loggs error on 400 and
+    500 series codes
+    """
 
-    r = requests.get(url)
-    status_code = str(r.status_code)
+    response = requests.get(url)
+    status_code = str(response.status_code)
 
-    if status_code.startswith("2"):
-        return True
-    else:
-        return False
+    if status_code.startswith("4") or status_code.startswith("5"):
+        logging.error('%s: %s', status_code, response.reason)
+
+    return status_code.startswith("2")
+
+
+"""
+If file is executed on itself then call a definition,
+mostly for testing purposes
+"""
+if __name__ == '__main__':
+    print(check_status_code('http://www.example.com/'))
