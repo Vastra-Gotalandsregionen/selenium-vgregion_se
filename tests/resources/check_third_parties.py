0c73bc3ee6087ae5fc01c11892e0150b21563cdb
Filip Dalväg <filip.dalvag@vgregion.se>
2020-11-04 13:30:48 +0100

    Lint on check third parties, and logging.

--- tests/resources/check_third_parties.py
+++ tests/resources/check_third_parties.py
@@ -1,24 +1,37 @@
+"""
+Check that common trackers are not used.
+"""
+
+import logging
 import requests
+
 
 def only_allowed_third_parties(url='https://1177.se'):
     """
-    Checks HTML for not allowed third parties. For te moment only looks for Google Analytics, Google Tag Manager and Adobe Analytics.
-    
+    Checks HTML for not allowed third parties. For te moment only looks for
+    Google Analytics, Google Tag Manager and Adobe Analytics.
+
     Attribute: 'url' as fully qualified URL, default is 'https://1177.se'
     """
-    r = requests.get(url, timeout=20)
-    
-    if 'ga.js' in r.text.lower() or 'www.google-analytics.com/analytics.js' in r.text.lower(): # Google Analytics
+
+    response = requests.get(url, timeout=20)
+
+    if 'ga.js' in response.text.lower() \
+       or 'www.google-analytics.com/analytics.js' in response.text.lower():
+        logging.error('Google Analytics is present')
         return False
-    elif 'gtm.js' in r.text.lower(): # Google Tag Manager
+    if 'gtm.js' in response.text.lower():
+        logging.error('Google Tag Manager is present')
         return False
-    elif '//assets.adobedtm.com/' in r.text.lower(): # Adobe Analytics
+    if '//assets.adobedtm.com/' in response.text.lower():
+        logging.error('Adobe Analytics is present')
         return False
-    
+
     return True
 
 """
-If file is executed on itself then call a definition, mostly for testing purposes
+If file is executed on itself then call a definition,
+mostly for testing purposes
 """
 if __name__ == '__main__':
-	print(only_allowed_third_parties('https://vgregion.se'))
+    print(only_allowed_third_parties('http://vgregion.se'))
