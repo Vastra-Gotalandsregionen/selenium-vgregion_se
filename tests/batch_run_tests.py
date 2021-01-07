a6aa157b86c3961f68f434511528584c9637e799
Filip Dalväg <filip.dalvag@vgregion.se>
2021-01-07 11:58:15 +0100

    Made the script a function to be able to call from outside with a list of specified files.

--- tests/batch_run_tests.py
+++ tests/batch_run_tests.py
@@ -4,19 +4,34 @@ Run all or some tests
 import glob
 import robot
 
-files = glob.glob("*.robot")
 
-files = ['hitta_dokument.robot',
-         'hitta_vgregion.robot',
-         'organisationssök.robot',
-         'search_page_listing_block.robot',
-         'sidlistningsblock.robot',
-         'sök_jobb_VGR.robot']
+def batch_run_tests(files=None):
+    """
+    Takes a list as an input listing files in the directory.
+    If none given it runs all .robot files instead.
+    """
 
-logFile = open('batch_log_output.txt', 'w')
+    if files is not None:
+        files = glob.glob("*.robot")
 
-for file in files:
-    print(file)
-    # Things done for each file
-    # robot.run(file + ' -d logs -o ' + file + ' -l' + file)
-    robot.run(file, stdout=logFile)
+    log_file = open('batch_log_output.txt', 'w')
+
+    for file in files:
+        print(file)
+        # Things done for each file
+        # robot.run(file + ' -d logs -o ' + file + ' -l' + file)
+        robot.run(file, stdout=log_file)
+
+
+"""
+If file is executed on itself then call a definition,
+mostly for testing purposes
+"""
+if __name__ == '__main__':
+    InputFiles = ['hitta_dokument.robot',
+                  'hitta_vgregion.robot',
+                  'organisationssök.robot',
+                  'search_page_listing_block.robot',
+                  'sidlistningsblock.robot',
+                  'sök_jobb_VGR.robot']
+    print(batch_run_tests(InputFiles))
