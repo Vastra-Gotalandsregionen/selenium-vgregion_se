8e3c43107619504eb1921d394b0d94afdcbdc3af
LucasOlssonPF <lucas.olsson@preciofishbone.se>
2021-01-11 15:57:46 +0100

    Update batch_run_tests.py
    Output formatet på testerna är i XUnit-format och mappar på servern skickas in som argument

--- tests/batch_run_tests.py
+++ tests/batch_run_tests.py
@@ -1,8 +1,23 @@
 """
 Run all or some tests
 """
+
+import argparse
+import getopt
 import glob
+import os
+import sys
+
 import robot
+from robot import rebot
+
+
+def parse_argument():
+        parser = argparse.ArgumentParser(description='Runs .robot files and creates output that can be used by Azure DevOps Pipelines')
+        parser.add_argument('--destdir', dest='destdir', required=True, help='Location of the .robot files that contains Selenium tests')
+        parser.add_argument('--outputdir', dest='outputdir', required=True, help='Output folder for the test results')
+        # return dictionary of args
+        return vars(parser.parse_args())
 
 
 def batch_run_tests(files=None):
@@ -10,6 +25,12 @@ def batch_run_tests(files=None):
     Takes a list as an input listing files in the directory.
     If none given it runs all .robot files instead.
     """
+
+    arguments = parse_argument()
+    seleniumDestDir = arguments['destdir']
+    outputLogDir = arguments['outputdir']
+    
+    os.chdir(seleniumDestDir)  
 
     if files is None:
         files = glob.glob("*.robot")
@@ -19,9 +40,14 @@ def batch_run_tests(files=None):
     for index, file in enumerate(files):
         print(file)
 
+        outputFileName = 'output_' +  file.replace(".robot", "")
+
         # Things done for each file
         # robot.run(file + ' -d logs -o ' + file + ' -l' + file)
-        robot.run(file, stdout=log_file)
+        robot.run(file, output=outputFileName, stdout=log_file)
+
+        # Create XUnit output
+        rebot(outputLogDir + outputFileName + '.xml', xunit='xunit' + outputFileName + '.xml')
 
         # To limit amount of tests (change to positive number)
         if index == -1:
