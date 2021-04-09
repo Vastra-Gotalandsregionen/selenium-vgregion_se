b9d48c0696ee4518b058c9857cbc04c82ad9cf2f
Filip Dalväg <filip.dalvag@vgregion.se>
2021-04-09 15:42:33 +0200

    test our domains.

---
+++ tests/Resources/test-domains.py
@@ -0,0 +1,32 @@
+"""
+See if our domains work
+"""
+import json
+from checkStatusCode import check_status_code
+
+def test_domains(file_path="../../domains.json"):
+    """
+    Reads a list of domains and see of they respond
+    """
+
+    # Read file
+    with open(file_path, 'r') as domain_file:
+        domains_json = domain_file.read()
+
+    # Parse file
+    domains = json.loads(domains_json)
+
+    results = {}
+
+    for domain in domains:
+        status = check_status_code(domain)
+        results[domain] = status
+
+    return results
+
+"""
+If file is executed on itself then call a definition,
+mostly for testing purposes
+"""
+if __name__ == '__main__':
+    print(test_domains(file_path="../../domains.json"))
